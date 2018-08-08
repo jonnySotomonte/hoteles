21344f773de48e8f46757ec223bcf3fa27f361f1
jonnysotomonteBdB <jsotomo@bancodebogota.com.co>
2018-08-08 10:04:56 -0500

    search by name with upperCase

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require("express"),
     app = express(),
-    bodyParser  = require("body-parser"),
+    bodyParser = require("body-parser"),
     methodOverride = require("method-override");
-    mongoose = require('mongoose');
-    request = require('request');
-    fs = require('fs');
-    underscore = require("underscore");
-    cors = require('cors');
+mongoose = require('mongoose');
+request = require('request');
+fs = require('fs');
+underscore = require("underscore");
+cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -17,30 +17,29 @@ var router = express.Router();
 var hoteles = JSON.parse(fs.readFileSync('./models/data.json'));
 
 router.get('/', function(req, res) {
-    console.log(hoteles);
     res.json(hoteles);
 });
 
-router.get('/byId', function(req, res){
+router.get('/byId', function(req, res) {
     var hotelId = req.query.id;
-    var hotel = underscore.where(hoteles, {id: hotelId});
+    var hotel = underscore.where(hoteles, { id: hotelId });
     res.send(hotel);
 });
 
-router.get('/byName', function(req, res){
-    var hotelId = req.query.name;
-    var hotelesPorNombre = underscore.filter(hoteles, function(hotel){ return hotel.name.includes(hotelId) });
+router.get('/byName', function(req, res) {
+    var hotelName = req.query.name.toUpperCase();
+    var hotelesPorNombre = underscore.filter(hoteles, function(hotel) { return hotel.name.toUpperCase().includes(hotelName) });
     res.send(hotelesPorNombre);
 });
 
-router.get('/byStars', function(req, res){
+router.get('/byStars', function(req, res) {
     var hotelStars = req.query.stars;
-    var hotelesPorEstrellas = underscore.where(hoteles, {stars: parseInt(hotelStars, 10)});
+    var hotelesPorEstrellas = underscore.where(hoteles, { stars: parseInt(hotelStars, 10) });
     res.send(hotelesPorEstrellas);
 });
 
 app.use(router);
 
 app.listen(3000, function() {
-  console.log("Node server running on http://localhost:3000");
+    console.log("Node server running on http://localhost:3000");
 });
